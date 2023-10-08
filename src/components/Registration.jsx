1c43e34cbf22c3488298064ba71aa1606c074ab7
Kabir <kabir.du17@gmail.com>
2023-10-08 08:36:23 +0600

    video 6

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,13 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const Registration = () => {
 
+    const { createUser } = useContext(AuthContext);
+
     const handleRegistration = e => {
         e.preventDefault();
-        const name = e.target.name.value;
+        // const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error.message)
+            })
     }
 
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Home from './components/Home';
 import Root from './components/root';
 import Login from './components/Login';
 import Registration from './components/Registration';
+import AuthProvider from './Providers/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -34,6 +35,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
